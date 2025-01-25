55311a5f82ae4e38de849fc2003cd2cffc026302
aliazgb <ali.azgb10@gmail.com>
2025-01-25 03:20:38 +0100

    Add Location List Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Header from "./components/Header/Header";
 import "./App.css";
-
+import LocationList from "./components/LocationList";
 function App() {
   return (
     <div>
       <Header />
+      <LocationList/>
     </div>
   );
 }
