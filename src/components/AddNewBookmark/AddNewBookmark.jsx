020ff77e15cc2964c2ee89943f0f955cf8807754
aliazgb <ali.azgb10@gmail.com>
2025-02-03 23:52:05 +0100

    new feuture give location when click on map

--- src/components/AddNewBookmark/AddNewBookmark.jsx
+++ src/components/AddNewBookmark/AddNewBookmark.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useUrlLocation from "../../hook/useUrlLocation";
 import { useNavigate } from "react-router-dom";
-
+import axios from "axios";
+const BASE_GEOCODING_URL =
+  "https://us1.api-bdc.net/data/reverse-geocode-client";
 function AddNewBookmark() {
-    const navigate=useNavigate()
+  const navigate = useNavigate();
   const [lat, lng] = useUrlLocation();
+  const [cityName, setCityName] = useState("");
+  const [country, setCountry] = useState("");
+  useEffect(() => {
+    async function fetchLocationData() {
+      try {
+        const { data } = await axios.get(
+          `${BASE_GEOCODING_URL}?latitude=${lat}&longitude=${lng}`
+        );
+      } catch (error) {}
+    }
+    fetchLocationData();
+  }, [lat, lng]);
   return (
     <div>
       <h2>Bookmark New Location</h2>
@@ -18,16 +32,16 @@ function AddNewBookmark() {
           <input type="text" name="cityName" id="CountryName" />
         </div>
         <div className="buttons">
-          <button className="btn btn--back">back</button>
           <button
-            className="btn btn--primary"
+            className="btn btn--back"
             onClick={(e) => {
               e.preventDefault();
-              navigate(``)
+              navigate(-1);
             }}
           >
-            Add
+            back
           </button>
+          <button className="btn btn--primary">Add</button>
         </div>
       </form>
     </div>
